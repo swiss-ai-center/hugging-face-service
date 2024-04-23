39d335fa78d7815688843af325766c4ba9a1f9a0
gaetan.cogliati <gaetan.cogliati@edu.hefr.ch>
2024-04-23 18:46:26 +0200

    Handle input/ouputs types for audio, image and text(json) and error handling from HF's API + description

--- src/main.py
+++ src/main.py
@@ -41,14 +41,12 @@ class MyService(Service):
 
     def __init__(self):
         super().__init__(
-            # TODO: 3. CHANGE THE SERVICE NAME AND SLUG
             name="Hugging Face",
             slug="hugging-face",
             url=settings.service_url,
             summary=api_summary,
             description=api_description,
             status=ServiceStatus.AVAILABLE,
-            # TODO: 4. CHANGE THE INPUT AND OUTPUT FIELDS, THE TAGS AND THE HAS_AI VARIABLE
             data_in_fields=[
                 FieldDescription(
                     name="json_description",
@@ -84,15 +82,7 @@ class MyService(Service):
         )
         self._logger = get_logger(settings)
 
-    # TODO: 5. CHANGE THE PROCESS METHOD (CORE OF THE SERVICE)
     def process(self, data):
-        # NOTE that the data is a dictionary with the keys being the field names set in the data_in_fields
-        # The objects in the data variable are always bytes. It is necessary to convert them to the desired type
-        # before using them.
-        # raw = data["image"].data
-        # input_type = data["image"].type
-        # ... do something with the raw data
-
         json_description = json.loads(data['json_description'].data.decode('utf-8'))
         api_token = json_description['api_token']
         api_url = json_description['api_url']
@@ -104,44 +94,65 @@ class MyService(Service):
                 file_name = zip_ref.namelist()[0]
                 return zip_ref.read(file_name)
 
-        def create_zip_from_json(json_data):
+        def is_valid_json(json_string):
+            try:
+                json.loads(json_string)
+                return True
+            except ValueError:
+                return False
+
+        def create_zip_from_bytes(file_bytes, file_name):
             zip_buffer = io.BytesIO()
             with zipfile.ZipFile(zip_buffer, "w") as zip_file:
-                zip_file.writestr("result.json", json.dumps(json_data, indent=4))
+                zip_file.writestr(file_name, file_bytes)
             zip_bytes = zip_buffer.getvalue()
             return zip_bytes
 
-        def natural_language_query(payload):
+        def json_input_query(payload):
             response = requests.post(api_url, headers=headers, json=payload)
-            return response.json()
+            return response
 
-        def audio_or_image_to_json_query(audio_data):
-            response = requests.request("POST", api_url, headers=headers, data=audio_data)
-            return json.loads(response.content.decode("utf-8"))
-
+        def audio_or_image_input_query(file_bytes):
+            response = requests.post(api_url, headers=headers, data=file_bytes)
+            return response
 
         zip_file_bytes = data['hugging_input'].data
         file_data = extract_file_from_zip(zip_file_bytes)
+        processed_data = None
+        result_data = None
+
         if json_description['input_type'] == FieldDescriptionType.APPLICATION_JSON:
             json_payload = json.loads(file_data.decode('utf-8'))
-            result_data = natural_language_query(json_payload)
-            return {
-                "result": TaskData(data=create_zip_from_json(result_data),
-                                   type=FieldDescriptionType.APPLICATION_ZIP)
-            }
-        elif json_description['input_type'] == FieldDescriptionType.AUDIO_MP3:
-            result_data = audio_or_image_to_json_query(file_data)
-            return {
-                "result": TaskData(data=create_zip_from_json(result_data),
-                                   type=FieldDescriptionType.APPLICATION_ZIP)
-            }
-        elif json_description['input_type'] == FieldDescriptionType.IMAGE_PNG or FieldDescriptionType.IMAGE_JPEG == \
+            result_data = json_input_query(json_payload)
+        elif json_description['input_type'] == FieldDescriptionType.AUDIO_MP3 or json_description['input_type'] == \
+                FieldDescriptionType.IMAGE_PNG or FieldDescriptionType.IMAGE_JPEG == \
                 json_description['input_type']:
-            result_data = audio_or_image_to_json_query(file_data)
-            return {
-                "result": TaskData(data=create_zip_from_json(result_data),
-                                   type=FieldDescriptionType.APPLICATION_ZIP)
-            }
+            result_data = audio_or_image_input_query(file_data)
+
+        if is_valid_json(result_data.content):
+            json_data = json.loads(result_data.content)
+            if 'error' in json_data:
+                self._logger.error(json_data['error'])
+                raise Exception(json_data['error'])
+
+        match json_description['output_type']:
+            case FieldDescriptionType.APPLICATION_JSON:
+                result_data = json.dumps(result_data.json(), indent=4)
+                processed_data = create_zip_from_bytes(result_data, "result.json")
+            case FieldDescriptionType.IMAGE_PNG:
+                processed_data = create_zip_from_bytes(result_data.content, "result.png")
+            case FieldDescriptionType.IMAGE_JPEG:
+                processed_data = create_zip_from_bytes(result_data.content, "result.jpg")
+            case FieldDescriptionType.AUDIO_MP3:
+                # TODO Find a way to convert this to mp3 as requested
+                processed_data = create_zip_from_bytes(result_data.content, "result.wav")
+            case FieldDescriptionType.AUDIO_OGG:
+                processed_data = create_zip_from_bytes(result_data.content, "result.ogg")
+
+        return {
+            "result": TaskData(data=processed_data,
+                               type=FieldDescriptionType.APPLICATION_ZIP)
+        }
 
 
 service_service: ServiceService | None = None
@@ -195,7 +206,41 @@ async def lifespan(app: FastAPI):
 
 
 # TODO: 6. CHANGE THE API DESCRIPTION AND SUMMARY
-api_description = """A service that uses Hugging Face's model hub API to directly query AI models
+api_description = """This service uses Hugging Face's model hub API to directly query AI models \n
+You can choose from any model available on the inference API from the [Hugging Face Hub](https://huggingface.co/models)
+
+This service has two inputs:
+ - A json file that defines the model you want to use, your access token and the input/output types you expect.
+ - A zip file containing the input file.
+ 
+json_description.json example:
+ ```
+ {
+    "api_token": "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",
+    "api_url": "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2",
+    "input_type": "application/json",
+    "output_type": "application/json"
+}
+```
+This specific model "roberta-base-squad2" was trained on question-answer pairs, including unanswerable questions, 
+for the task of Question Answering.
+
+The input looks like this:
+
+```json
+{
+   "inputs":{
+      "question":"What is my name?",
+      "context":"My name is Clara Postlethwaite and I live in Berkeley."
+   }
+}
+```
+This is an example, check the model hub to see what the input of the model you want to use looks like.
+Don't forget to compress it before giving it to the service!
+
+The model may need some time to load on Hugging face's side, you may encounter an error on your first try.
+Helpful trick: The answer from the inference API is cached, so if you encounter a loading error make sure to change the
+input ever so slightly.
 """
 api_summary = """A service that uses Hugging Face's model hub API to directly query AI models
 """
